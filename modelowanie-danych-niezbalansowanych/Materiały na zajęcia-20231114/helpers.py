c250e0d373c6a00a7735eb7fa6c312b8b4ea400f
Mieczmik <mikolajmiecznikowski94@gmail.com>
2023-11-14 16:35:57 +0100

    first commit

---
+++ modelowanie-danych-niezbalansowanych/Materiały na zajęcia-20231114/helpers.py
@@ -0,0 +1,125 @@
+import time
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from operator import itemgetter
+
+# Dodatkowo, wyłączymy warningi, które pojawiają się w najnowszej wersji:
+xgb.set_config(verbosity = 0)
+
+def run_xgb(train, validate, features, target, # argumenty obowiązkowe
+            eta=0.03, max_depth=7, subsample = 0.7, colsample_bytree = 0.7, # hiperparametry
+            colsample_bylevel=1,lambdaX = 1, alpha=0, gamma=0, min_child_weight=0, # hiperparametry
+            rate_drop = 0.2, skip_drop=0.5, # hiperparametry
+            num_boost_round = 1000, early_stopping_rounds = 50, # hiperparametry
+            debug=True, eval_metric= ["auc"], objective = "binary:logistic", # konfiguracja
+            seed=2017, booster = "gbtree", tree_method="exact", grow_policy="depthwise"): # konfiguracja
+    '''
+    Uniwersalny wrapper dla XGB dla gbtree i dart
+    Więcej opcji https://xgboost.readthedocs.io/en/stable/parameter.html
+    
+    Parametry
+    ----------
+    train, validate, features, target : wymagane zmienne bez domyślnych wartości
+    train, validate : pd.DataFrames z kolumnami opisanymi w features i target
+    features : lista zmiennych do wykorzystania w trenowaniu
+    target : nazwa zmiennej objasnianej
+    
+    --- Zmienne wspólne dla gbtree i dart
+        --- Zmienne właściwe dla Ensemble/Boosting
+        eta : "learning rate"
+        max_depth=7 : maksymalna głębokość drzew [0,∞]
+        subsample : udział (0,1] obserwacji do treningu jednej iteracji
+        colsample_bytree : udział (0,1] kolumn do treningu jednej iteracji
+        colsample_bylevel : udział  (0,1] kolumn na poziom do treningu jednej iteracji
+        --- Zmienne regularyzacyjne
+        lambdaX=0 : regularyzacja L2 [0,∞]
+        alpha=0 : regularyzacja L1 [0,∞]
+        gamma=1 : minimalna redukcja funkcji straty
+        min_child_weight=0 : minimalna suma wag poddrzewa
+
+    --- Zmienne dla algorytmu dart
+    rate_drop : 
+    skip_drop : 
+    
+    --- Zmienne dla XGB, opis/agorytm/liczba drzew etc.
+    num_boost_round : maksymalna liczba iteracji
+    early_stopping_rounds : margines iteracji dla early stopping
+    debug : Czy włączyć pełne opisy.
+    eval_metric : Pełna lista dostępna https://github.com/dmlc/xgboost/blob/master/doc/parameter.md 
+    objective : reg:linear, reg:logistic, binary:logistic, multi:softmax lub inne Pełna lista dostępna https://xgboost.readthedocs.io/en/stable/parameter.html
+    seed : random seed
+    booster : silnik dla drzew gbtree (cart), dart (gbtree z dropoutem) lub gblinear
+    tree_method : ‘auto’, ‘exact’, ‘approx’, ‘hist’, ‘gpu_exact’, ‘gpu_hist’ zobacz http://xgboost.readthedocs.io/en/latest/parameter.html
+    grow_policy : depthwise, lossguide
+    '''
+   
+    start_time = time.time()
+    paramList= ['eta', 'max_depth',
+         'subsample', 'colsample_bytree', 'colsample_bylevel',
+         'lambdaX', 'alpha', 'gamma', 'min_child_weight',
+         'num_boost_round', 'early_stopping_rounds',
+         'rate_drop', 'skip_drop',
+         'eval_metric', 'objective', 
+          'seed', 'booster', 'tree_method', 'grow_policy']
+    
+    # Stworzenie słownika do przekazania do XGB
+    params = dict()
+    for param in paramList:
+        params[param]=eval(param)
+    if(debug):
+        for param in paramList:
+            print(param, eval(param), end=", ")
+        print('\nLength train:', len(train.index))
+        print('Length valid:', len(validate.index))
+
+    # Automatyczne przekazanie liczby klas dla wielu poziomów klasyfikacji
+    if (params["objective"]=="multi:softmax" or params["objective"]=="multi:softprob"):
+        params["num_class"]=train[target].nunique()
+    params["silent"]=1
+    
+    # XGB wymaga w słowniku parametrów słowa kluczowego lambda, poniżej workaround
+    params["lambda"]=lambdaX
+    
+    # Przekształcenie zbiorów do struktury DMatrix
+    # Struktura danych DMatrix pozwala na efektywne tworzenie drzew
+    dtrain = xgb.DMatrix(train[features].values, train[target].values, feature_names=train[features].columns.values)
+    dvalid = xgb.DMatrix(validate[features].values, validate[target].values, feature_names=validate[features].columns.values)
+
+    # Stworzenie listy zbiorów do ealuacji
+    evals = [(dtrain, 'train'), (dvalid, 'valid')]
+    
+    # Stworzenie zmiennej słownikowej do zapisania historii dopasowania modelu 
+    train_history = dict()
+
+    # Uruchomienie algorytmu trenującego
+    gbm = xgb.train(params, dtrain, 
+                    num_boost_round, early_stopping_rounds=early_stopping_rounds,
+                    evals=evals, evals_result=train_history, verbose_eval=debug)
+    
+    # Obliczenia statystyk i dodatkowych wartości
+    score = gbm.best_score
+
+    # Przekształcenie historii trenowania do Pandas Data Frame
+    trainHistory = dict()
+    for key in train_history.keys():
+        for metric in train_history[key].keys():
+            trainHistory[key+metric.upper()]=train_history[key][metric]
+    train_history = pd.DataFrame(trainHistory)
+
+    # Zapis przewidywanych wartości dla zbioru walidacyjnego dla najlepszej (z punktu widzenia walidacji) iteracji
+    trainPred = gbm.predict(dtrain, iteration_range=(0, gbm.best_iteration+1))
+    testPred = gbm.predict(dvalid, iteration_range=(0, gbm.best_iteration+1))
+
+    # Przygotowanie posortowanej listy dla wazności zmiennych, zamiast słownika
+    imp = gbm.get_score(importance_type='total_gain')
+    imp = sorted(imp.items(), key=itemgetter(1), reverse=True)
+
+    impFig=None
+    if(debug):
+        print('Czas trenowania: {} minut'.format(round((time.time() - start_time)/60, 2)))
+        # Skorzystanie z wbudowanej funkcji wizualizującej waznośc zmiennych
+        impFig, ax = plt.subplots()
+        xgb.plot_importance(gbm, ax=ax)
+    return score, trainPred, testPred, train_history, impFig, imp
